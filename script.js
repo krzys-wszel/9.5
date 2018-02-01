de01ad3c3ee386f093e61c9ab6f5cce72651a403
Krzysztof Wszelaki <krzys_wszel@interia.eu>
2018-02-01 20:25:08 +0100

    9.5

---
+++ script.js
@@ -0,0 +1,5 @@
+var withButtonClass = document.getElementsByClassName("button");
+console.log(withButtonClass);
+for (i = 0; i < withButtonClass.lenght; i++) {
+    console.log(withButtonClass[i].innerText);
+}
